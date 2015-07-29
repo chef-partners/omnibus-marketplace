b68945c4003113156668366796566fbd6c81bdcd
Ryan Cragun <me@ryan.ec>
2015-07-29 16:45:44 -0700

    Write out a chef-marketplace-running.json during the reconfigure

--- files/chef-marketplace-cookbooks/chef-marketplace/recipes/default.rb
+++ files/chef-marketplace-cookbooks/chef-marketplace/recipes/default.rb
@@ -1,1 +1,8 @@
 include_recipe 'chef-marketplace::enable'
+
+file '/etc/chef-marketplace/chef-marketplace-running.json' do
+  content Chef::JSONCompat.to_json_pretty('chef-marketplace' => node['chef-marketplace'])
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
