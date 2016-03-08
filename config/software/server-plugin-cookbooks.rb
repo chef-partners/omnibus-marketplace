3710b0650c058254d81e6c370fdb5f6f62ebef0e
Ryan Cragun <me@ryan.ec>
2016-03-08 11:30:24 -0800

    Pin berks and remove unneeded vendoring

--- config/software/server-plugin-cookbooks.rb
+++ config/software/server-plugin-cookbooks.rb
@@ -2,13 +2,6 @@ name 'server-plugin-cookbooks'
 
 source path: "#{project.files_path}/#{name}"
 
-dependency 'berkshelf'
-
 build do
-  command "#{install_dir}/embedded/bin/berks vendor #{install_dir}/embedded/server-plugin-cookbooks --berksfile=./chef-marketplace/Berksfile",
-          env: { 'RUBYOPT'         => nil,
-                 'BUNDLE_BIN_PATH' => nil,
-                 'BUNDLE_GEMFILE'  => nil,
-                 'GEM_PATH'        => nil,
-                 'GEM_HOME'        => nil }
+  sync project_dir, "#{install_dir}/embedded/server-plugin-cookbooks/"
 end
