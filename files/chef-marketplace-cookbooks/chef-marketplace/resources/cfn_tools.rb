1a330dfc93d36c1ec8ecd78f34f256efb2a29c8a
Ryan Cragun <me@ryan.ec>
2017-10-26 15:10:10 -0700

    [CLOUD-540] Install Cloudformation tools on AWS images
    As we transition our AWS offerings to use CloudFormation we'll need to
    properly signal the stack when setup is completed. During publishing on
    we now ensure that the tools are installed and linked.

---
+++ files/chef-marketplace-cookbooks/chef-marketplace/resources/cfn_tools.rb
@@ -0,0 +1,67 @@
+require 'uri'
+
+actions :install
+default_action :install
+
+property :src_url,
+  String,
+  default: 'https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz'
+
+action :install do
+  unless tools_installed?
+    converge_by 'install cloudformation tools' do
+      package 'epel-release'
+      package 'python-pip'
+
+      directory '/opt/aws/bin' do
+        recursive true
+      end
+
+      remote_file '/tmp/cfn.tar.gz' do
+        source src_url
+      end
+
+      bash 'build and install tools' do
+        cwd "/tmp"
+        code <<-EOH
+          tar xpf cfn.tar.gz
+          cd aws-cfn-bootstrap-*
+          python setup.py build
+          python setup.py install
+          ln -s /usr/init/redhat/cfn-hup /etc/init.d/cfn-hup
+          chmod 775 /usr/init/redhat/cfn-hup
+        EOH
+      end
+
+      cfn_binary_names.each do |cmd|
+        link ::File.join("/opt/aws/bin", cmd) do
+          to ::File.join("/usr/bin", cmd)
+        end
+      end
+    end
+  end
+end
+
+action_class do
+  def cfn_binary_names
+    %w(
+      cfn-hup
+      cfn-init
+      cfn-signal
+      cfn-elect-cmd-leader
+      cfn-get-metadata
+      cfn-send-cmd-event
+      cfn-send-cmd-result
+    )
+  end
+
+  def tools_installed?
+    cfn_binary_names.all? do |t|
+      begin
+        ::File.executable?(::File.join('/opt/aws/bin', t))
+      rescue
+        false
+      end
+    end
+  end
+end
